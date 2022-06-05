f70d4b52941ec3104dfb64d5dc60cd93f2b7ada2
Ian Stedman <iancstedman@gmail.com>
2022-06-05 21:32:43 +0100

    Add files via upload
    This fixes multiple issues:
    1) If the unit was externally powered (not via BEC), there was no horn input and it looked for <VALUE to operate, the horn would continually sound.
    2) The gear change was like a dodgy automatic, going up/down around the trigger points. Moving average filter + hysteresis fixed this.
    3) Sometimes the 'advert' used to play the horn, was cut off, added new code to fix this, now using a 13 second delay. Will improve in next release.
    4) Reduced the loop time from ~250ms to 32ms, this helped the averaging filter respond quicker and is more efficient. The Pulsein() function is a paint.

---
+++ RCSounds_V1.2_test/RC_Sounds.h
@@ -0,0 +1,142 @@
+/*
+ * RC_Sounds.h
+ * This provides all the configuration for the RC_Sounds software.
+ */
+
+
+#define LEDPIN        PB2
+#define HORN_IN       PB0
+#define SELECTION_IN  A0
+#define THROTTLE_IN   PB1
+#define LEDON         LOW
+#define LEDOFF        HIGH
+#define SWITCHCLOSED  0x77
+#define SWITCHOPEN    0x66
+
+
+/*
+ * These defines might seem extraneous but they allow easier configuration if the code is used for Boats, Cars/Trucks
+ * or trains. They are compile time directives and only the code defined gets built.
+ */
+
+#define BOAT
+//#define CAR
+/* Constants for Sound effect selection these are the scaled voltages seen at the input with +/-0.15V tolerance.
+ *  All calculated values for reference
+ *  Switch(es)  Equivalent resistance   Upper limit   Nominal   Lower limit
+ *  None        100K                    4.69V         4.54V     4.39V
+ *  S4          31.972K                 4.05V         3.90V     3.75V
+ *  S3          15.27K                  3.50V         3.35V     3.20V
+ *  S2          9.09K                   2.65V         2.50V     2.35V            
+ *  S1          4.48K                   1.85V         1.70V     1.55V
+ *  S5          2.15K                   1.03V         0.88V     0.73V
+ *  S4+S3                               3.15V         3.00V     2.85V    
+ *  S4+S2                               2.50V         2.35V     2.20V
+ *  S3+S2                               2.30V         2.15V     2.00V       
+ *  
+ */
+#ifdef BOAT
+const float NAVY_DIESEL_UPPER =  4.69;   
+const float NAVY_DIESEL_LOWER =  4.39;    // All switches open
+const float TUG_DIESEL_UPPER  =  4.05;    // S4 only closed
+const float TUG_DIESEL_LOWER  =  3.75;    
+const float STEAM_1_UPPER     =  3.50;    // S3 only closed
+const float STEAM_1_LOWER     =  3.20;    
+const float STEAM_2_UPPER     =  2.65;    // S2 only closed
+const float STEAM_2_LOWER     =  2.35;
+
+//const int MIN_PULSE=750;
+//const int MAX_PULSE=2500;
+
+enum Sound_Effect_Type{NavyDiesel=0,TugDiesel=1,SteamBoat1=2,SteamBoat2=3,None=99};
+/*
+ * Multi-dimensional array to pick the sound effect played. 
+ * Column 0 is the default set of sounds i.e. idle, column 1 is 1st gear, column 2 is 2nd gear, column 3 is 3rd gear, column 4 is 4th gear
+ * Row 0 is Navy diesel, Row 1 is Tug diesel, Row 2 is Steam boat, Row 3 is Steam launch.
+ * This same array, used as a lookup table, also selects the horn sound.
+ * So Navy diesel sound #5, for example, would select Advert #5 if needed, which blends sample #5 with the horn
+ */
+byte SpeedIndex[4][5]={
+                        {1,2,3,4,5},        // Navy diesel sounds
+                        {6,7,8,9,10},       // Tug diesel sounds
+                        {11,12,13,14,15},   // Steam boat 1 sounds
+                        {16,17,18,19,20}    // Steam boat 2 sounds
+};
+#endif
+/*
+ * This sets the samples/type for Car/truck sounds
+ */
+#ifdef CAR
+
+
+enum Sound_Effect_Type{none=0,RegularCar,V8Car,MuscleCar,DieselTruck};
+
+#endif 
+
+
+/* The setup switch, S5 is regardless of output type and the values shown allow for S1-S4 closed in paralllel
+   or just S5 closed on its own.
+*/
+#ifdef DEBUGPROG
+const float SETUP_UPPER       =  1.03;
+const float SETUP_LOWER       =  0.49;
+//Test settings using S4 closed and push switch across S3, save changing fuses for testing.
+#else
+const float SETUP_UPPER       =  3.15;
+const float SETUP_LOWER       =  2.85;
+#endif
+
+
+enum Sound_Effect_Type Sounds=None;
+//Globals to hold the ADC sampled value and the converted voltage.
+float Scaled_V;
+//Used when reading servo pulse input
+unsigned int HornPulseLimit=0;
+unsigned int FirstGear,SecondGear,ThirdGear,FourthGear;
+unsigned int ForwardThrot, NeutralThrot, ReverseThrot;
+byte HornGreatOrLessThan, ThrotNormReverse;
+byte CurrentGear;
+// Set the hysteresis of the gear change
+const byte GearHysteresis=25;
+
+
+// To control the Advert timing
+byte CurrentSound,PreviousSound;
+byte CurrentAdvert=0,PreviousAdvert=0;
+byte AdvertPlaying=0;
+unsigned long AdTime=0;
+// Globals that hold the current PWM values
+unsigned int ThrottlePWM,HornPWM;
+//Global that denotes what signals are present
+byte PWMFound;
+
+const byte numReadings=4;
+unsigned int readings[numReadings];
+byte readIndex=0;
+unsigned int total=0;
+
+//Throttle globals
+unsigned int ThrottlePosition=0;
+// Settings related to servo pulse positions
+#define GREATERTHAN             0x33
+#define LESSTHAN                0x22
+#define NORMAL_THROT            0x34
+#define REVERSE_THROT           0x23
+#define HORN_EEPROM_LO          0
+#define HORN_EEPROM_HI          1
+#define HORN_EEPROM_GT_LT       2
+#define THROT_EEPROM_FORWARD_LO 3
+#define THROT_EEPROM_FORWARD_HI 4
+#define THROT_EEPROM_NEUTRAL_LO 5
+#define THROT_EEPROM_NEUTRAL_HI 6
+#define THROT_EEPROM_REVERSE_LO 7
+#define THROT_EEPROM_REVERSE_HI 8
+#define THROT_EEPROM_DIRECTION  9
+// For EEPROM setup
+#define E_INIT 256
+
+
+
+#define TimerAdjustment 27   //27us to allow for servo pulse jitter and detent of radio
+
+#define DEBOUNCETIME 200
